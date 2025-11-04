10b4a8cb448188a7d2d209079531dd8282b6c6b5
pedro.nonato <like.nonato.marrara@gmail.com>
2025-11-04 18:01:16 -0300

    feat: criando componente dateInput

---
+++ src/components/ui/DateInput/index.jsx
@@ -0,0 +1,21 @@
+import { useId } from "react";
+
+export default function DateInput({ label, value, onChange }) {
+    const id = useId();
+
+    return (
+        <div className="flex flex-col relative">
+            <label htmlFor={id} className="absolute top-[-10px] left-[12px] bg-white px-[4px] text-black text-[16px]">
+                {label}
+            </label>
+            <input
+                type="date"
+                name={id}
+                id={id}
+                value={value}
+                className="w-[360px] px-[12px] pt-[12px] pb-[4px] border-[1px] border-solid border-gray-300 rounded-md text-gray-600 text-[18px] focus:outline-none"
+                onChange={event => onChange(event.target.value)}
+            />
+        </div>
+    );
+}
