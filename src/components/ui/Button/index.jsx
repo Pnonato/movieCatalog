6ce25d0c465a72aaa9962de696b8ad8c66457831
pedro.nonato <like.nonato.marrara@gmail.com>
2025-11-04 18:01:08 -0300

    feat: criando componente button

---
+++ src/components/ui/Button/index.jsx
@@ -0,0 +1,20 @@
+export default function Button({
+    children,
+    color = "blue",
+    disabled = false,
+    onClick = () => {
+        return;
+    },
+}) {
+    return (
+        <button
+            type="button"
+            className={`w-[120px] h-[40px] rounded-md text-white text-[18px] self-center transition duration-300 ease-in-out hover:scale-105`}
+            style={{ backgroundColor: disabled ? "gray" : color }}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+}
