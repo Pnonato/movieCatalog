12287196d8d0cdfc52fd99ca7f239e8e55a20f30
pedro.nonato <like.nonato.marrara@gmail.com>
2025-11-04 18:00:52 -0300

    feat: criando componente Footer

---
+++ src/components/ui/Footer/index.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="mt-[16px] relative inset-x-0 bottom-0 text-center text-emerald-400 text-[18px] mb-4">
+            Developed with &#9829; by Pedro Nonato & Lucas Lotumolo
+        </footer>
+    );
+}
